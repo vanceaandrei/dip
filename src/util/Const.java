47449efb322f20da15f2b58ddd431c8cef1b4e79
vanceaandrei <vancea.andrei94@yahoo.ro>
2016-11-04 14:51:56 +0200

    first commit

---
+++ src/util/Const.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author andrei.vancea
+ */
+public class Const {
+
+    public static final ImageIcon picture = new ImageIcon("poza.png");
+}
